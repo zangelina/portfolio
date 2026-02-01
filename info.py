4e21ee908634f08314c0585a100fc7a11ffa6d09
zangelina <58609649+zangelina@users.noreply.github.com>
2026-02-01 04:04:30 -0500

    Revise portfolio details and academic information
    Updated personal information, education, experience, projects, and programming skills.

--- info.py
+++ info.py
@@ -3,7 +3,7 @@
 
 #CHANGE BELOW
 profile_picture = "Images/misery.jpeg"
-about_me = "I'm Angelina Zhang. I'm a first-year Applied Physics major! In the spirit of physics, this Lab03 project will be astronomy-related."
+about_me = "I'm Angelina Zhang. I'm a third-year Physics & CS double major, with a Philosophy minor! I hope you like my (outdated) portfolio, which just includes the projects I have had to complete for CS courses. Please navigate to Projects to see my GT Movies Store information (Hey, I did change like a few things so it isn't entirely outdated.)"
 
 
 #CHANGE BELOW (OPTIONAL)
@@ -18,31 +18,35 @@ my_email_address = "azhang421@gatech.edu"
 
 
 education_data ={
-    'Degree': 'Bachelor of Science in Applied Physics',
+    'Degree': 'Bachelor of Science in Physics',
     'Institution': 'Georgia Institute of Technology',
     'Location': 'Atlanta, GA',
     'Graduation Date': 'May 2027',
-    'GPA': '4.0'
+    'GPA': '3.90'
 }
 course_data = {
-    "code":["CS 1301", "PHYS 2231", "PHIL 4803", "GT 1000", "PSYC 1101"], 
-    "names":["Intro to CS - Python", "Honors Physics I", "AI Ethics and Policy", "First-Year Seminar - Physics", "Intro to Psychology"], 
-    "semester_taken":["1st", "1st", "1st", "1st", "1st"],
-    "skills":["I love simulating black holes.", "WHAT IS A KILOMETER?", "Should metal have human rights", "Superconductors Levitating (by Dua Lipa)", "How to manipulate people for personal gain"],
+    "code":["PHYS 4143", "CS 2110", "PHIL 4803", "PHIL 3103"], 
+    "names":["Quantum Mechanics II", "Computer Organization and Programming", "AI Ethics and Policy", "Modern Philosophy"], 
+    "semester_taken":["6th", "5th", "1st", "6th"],
+    "skills":["Poisson, wave functions, and misery.", "p- and n-junctions", "Should metal have human rights", "The rise of stoics in our society"],
     }
 experience_data = {
-    "Cashier at Panera Bread": (["- Best Employee",
-                                                                          "- Handled a lot of money every day", "- Had lots of leftover cookies, treats, and food (yum)"],"Images/panera.png")
+    "Cashier at Panera Bread": (["- Best Employee", "- Handled a lot of money every day", "- Had lots of leftover cookies, treats, and food (yum)"],"Images/panera.png")
 }
 
 projects_data = {
-    "CS 1301 Lab03, Solar System": "Find out how much you would weigh and how old you would be considered if you relocated to one of the other planets in our Solar System!", "Speckle Imaging Portfolio": "Imaged Jupiter using Julia Programming Language", "Telescope in the Sky": "Amateur astrophotography!"
+    "CS 2340 GT Movies Store": "The GT Movies Store website is a nifty 
+    way to browse a selection of (the best) movies. You have an About page (about the website), a Movies page (where you can find some of my favorite movies or movies that I just thought of), and a Cart page
+    (where you can see what movies, if any, you've added to your cart). You can also Login (or Sign Up!) if you'd like your data to be saved.
+    On my end, I'm able to have special admin privileges to manage the website, edit orders, view accounts, and more. This website allows you to review movies, edit and delete your own reviews, and report anyone's review,
+    regardless of whether you have an account or not. \n\n Process Description: This was a pretty difficult project, since I am not an HTML pro! Even following the guide, I struggled, as I was constantly getting errors from weird spaces and indentations in the provided code. Debugging took most of my time! Ed Discussion, Stack Overflow, and GeeksforGeeks were great. I started early and spread out my work, which went well for me. My methodology was to just thoroughly check all my steps, try my best to understand what was going on, narrate my thoughts, and take moments to logically parse what I was doing.",
+    "CS 1301 Lab03, Solar System": "Find out how much you would weigh and how old you would be considered if you relocated to one of the other planets in our Solar System!"
 }
 
 programming_data = {
     "Python": 100,
     "Java": 70,
-    "C++": 0
+    "C++": 60
 }
 
 #CHANGE BELOW (OPTIONAL)
@@ -63,7 +67,7 @@ spoken_data = {
     "Spanish": "Intermediate",
 }
 leadership_data = {
-    "President of Tri-M Music Honor Society": (["- Helped raise over $2,500 for charity."],"Images/catrinet.jpeg"), "Vice President of Model UN": (["- Hosted largest high school conference in GA"], "Images/mun.jpeg")
+    "President of the Society of Physics Students": (["- Vice President (2025)."],"Images/catrinet.jpeg"), "CS1301 Head Teaching Assistant": (["- Exam Lead (2025)"])
 
 }
 activity_data={

2e3f6384132cba045306ad6adce27be38ec5c023
zangelina <58609649+zangelina@users.noreply.github.com>
2026-02-01 03:41:29 -0500

    Add files via upload

---
+++ info.py
@@ -0,0 +1,72 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "images/misery.jpeg"
+about_me = "I'm Angelina Zhang. I'm a first-year Applied Physics major! In the spirit of physics, this Lab03 project will be astronomy-related."
+
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url = "https://www.linkedin.com/in/angelina-zhang-142a76217/"
+my_github_url = "https://github.com/zangelina"
+my_email_address = "azhang421@gatech.edu"
+
+
+education_data ={
+    'Degree': 'Bachelor of Science in Applied Physics',
+    'Institution': 'Georgia Institute of Technology',
+    'Location': 'Atlanta, GA',
+    'Graduation Date': 'May 2027',
+    'GPA': '4.0'
+}
+course_data = {
+    "code":["CS 1301", "PHYS 2231", "PHIL 4803", "GT 1000", "PSYC 1101"], 
+    "names":["Intro to CS - Python", "Honors Physics I", "AI Ethics and Policy", "First-Year Seminar - Physics", "Intro to Psychology"], 
+    "semester_taken":["1st", "1st", "1st", "1st", "1st"],
+    "skills":["I love simulating black holes.", "WHAT IS A KILOMETER?", "Should metal have human rights", "Superconductors Levitating (by Dua Lipa)", "How to manipulate people for personal gain"],
+    }
+experience_data = {
+    "Cashier at Panera Bread": (["- Best Employee",
+                                                                          "- Handled a lot of money every day", "- Had lots of leftover cookies, treats, and food (yum)"],"images/panera.png")
+}
+
+projects_data = {
+    "CS 1301 Lab03, Solar System": "Find out how much you would weigh and how old you would be considered if you relocated to one of the other planets in our Solar System!", "Speckle Imaging Portfolio": "Imaged Jupiter using Julia Programming Language", "Telescope in the Sky": "Amateur astrophotography!"
+}
+
+programming_data = {
+    "Python": 100,
+    "Java": 70,
+    "C++": 0
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons = {
+    "Python": "🐍",
+    "Java": "☕",
+    "C": "🔍",
+}
+spoken_icons = {"Mandarin Chinese": "🇨🇳",
+    "English": "🇬🇧",
+    "Spanish":"🇪🇸"
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "English": "Fluent",
+    "Mandarin": "Native",
+    "Spanish": "Intermediate",
+}
+leadership_data = {
+    "President of Tri-M Music Honor Society": (["- Helped raise over $2,500 for charity."],"images/catrinet.jpeg"), "Vice President of Model UN": (["- Hosted largest high school conference in GA"], "images/mun.jpeg")
+
+}
+activity_data={
+    "Society of Physics Students": ["- Host physics panels", 
+            "- Hold a formal", "- Biggest dough collector"]
+}
